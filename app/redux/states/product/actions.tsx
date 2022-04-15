92c77f7bf1a3b3e04a1db7fbbd7a7e881b326d9c
lcanutm <lcanutm@gmail.com>
2022-04-15 01:58:25 -0400

    cambios

--- app/redux/states/product/actions.tsx
+++ app/redux/states/product/actions.tsx
@@ -2,6 +2,6 @@ import { IProduct } from './state'
 
 export const UPDATE_PRODUCT = '[Product] update'
 
-export const updateAnnouncement = (products: IProduct[]) => dispatch => {
+export const updateProducts = (products: IProduct[]) => dispatch => {
   return dispatch({ type: UPDATE_PRODUCT, products })
 }
